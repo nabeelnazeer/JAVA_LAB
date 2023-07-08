917a3ad913ddee5f522cd583d80824e90530f493
Nabeel-nazeer <nabeel777@ug.cusat.ac.in>
2023-07-08 18:31:24 +0530

    seven done

--- JAVA_LAB 1/question 7/Main.java
+++ JAVA_LAB 1/question 7/Main.java
@@ -13,18 +13,18 @@ abstract class Account{
         this.accountBalance = balance;
     }
     
-    public void depositAmount(double amount) {
+    public void deposit(double amount) {
          accountBalance += amount;
          System.out.println("Amount successfully deposited.");
          System.out.println("New balance : " + accountBalance);
     }
 
-    public abstract void withdrawAmount(double amount);
+    public abstract void withdrawal(double amount);
 
     public void displayAccount() {
-        Systme.out.println("Acoount holder name: " + holderName);
-        Systme.out.println("Account no. : " + accNo);
-        Systme.out.println("Balance: " + accountBalance);
+        System.out.println("Acoount holder name: " + holderName);
+        System.out.println("Account no. : " + accountNo);
+        System.out.println("Balance: " + accountBalance);
     }
     
 }
@@ -36,16 +36,15 @@ class SavingsAccount extends Account{
         super(holderName,accNo,balance);
     }
 
-    @override
+    
 
-    public void withdrawAmount(double amount) {
+    public void withdrawal(double amount) {
         
-        if (balance - amount <= MIN_BALANCE) {
-            balance -= amount;
+        if (accountBalance - amount >= MIN_BALANCE){
+            accountBalance -= amount;
             System.out.println("Amount withdrawn successfully");
-            System.out.println("New balance: " + balance);
-        }
-        else {
+            System.out.println("New balance: " + accountBalance);
+        }else{
             System.out.println("Insufficient balance");
         }
     }
@@ -59,13 +58,13 @@ class CurrentAccount extends Account{
     }
 
 
-    @override
-    public void withdrawAmount(double amount) {
-        double overdraftAmount = balance * OVERDRAFT;
-        if (balance - amount >= -overdraftAmount) {
-            balance -= amount;
+    
+    public void withdrawal(double amount) {
+        double overdraftAmount = accountBalance * OVERDRAFT;
+        if (accountBalance - amount >= -overdraftAmount) {
+            accountBalance -= amount;
             System.out.println("Amount withdrawn successfull");
-            System.out.println("New balance: " + balance );
+            System.out.println("New balance: " + accountBalance );
         }
         else {
             System.out.println("Overdraft limit exceeded");
@@ -91,6 +90,8 @@ public class Main{
             System.out.print("Enter your choice");
             choice = scanner.nextInt();
 
+            scanner.nextLine();
+
             switch(choice) {
                 case 1:
                     createSavingAccount(scanner);
@@ -106,7 +107,7 @@ public class Main{
                     break;
                 case 5:
                     displayAccountDetails(scanner);
-                    break
+                    break;
                 case 0:
                     System.out.println("Thank you");
                     break;
@@ -116,10 +117,120 @@ public class Main{
             } 
 
 
-        } while (!choice == 0);
+        } while (!(choice == 0));
     }
     private static void createSavingAccount(Scanner scanner){
+        System.out.println("Enter account holder name : ");
+        String holderName = scanner.nextLine();
+
+        System.out.println("Enter Account number : ");
+        int accNo = scanner.nextInt();
+        scanner.nextLine();
+
+        if (checkAccountNumber(accNo)){
+            System.out.println("The Account number already exists");
+            return;
+        }
+
+        System.out.println("Enter the initial balnace : ");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+
+        Account account = new SavingsAccount(holderName,accNo,balance);
+        accounts.add(account);
+        System.out.println("Account created successfully!");
+
+    }
+    private static void createCurrentAccount(Scanner scanner){
+        System.out.println("Enter account holder name : ");
+        String holderName = scanner.nextLine();
+
+        System.out.println("Enter Account number : ");
+        int accNo = scanner.nextInt();
+        scanner.nextLine();
+
+        if (checkAccountNumber(accNo)){
+            System.out.println("The Account number already exists");
+            return;
+        }
+
+        System.out.println("Enter the initial balnace : ");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+
+        Account account = new SavingsAccount(holderName,accNo,balance);
+        accounts.add(account);
+        System.out.println("Account created successfully!");
+
+    }
+    private static void depositAmount(Scanner scanner){
+        System.out.println("Enter the account number: ");
+        int accNo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter Deposit Amount: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
         
+
+        Account account = getAccountNumber(accNo);
+        if (account != null){
+            account.deposit(amount);
+        }
+        else{
+            System.out.println("Account Does not exist");
+        }
+    }
+
+    private static void withdrawAmount(Scanner scanner){
+        System.out.println("Enter the account number: ");
+        int accNo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the amount to withdraw");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        Account account = getAccountNumber(accNo);
+        if (account != null){
+            account.withdrawal(amount);
+        }
+        else{
+            System.out.println("Account does not exist");
+        }
+    }
+
+    private static void displayAccountDetails(Scanner scanner){
+        System.out.print("Enter Account Number: ");
+        int accNo = scanner.nextInt();
+        scanner.nextLine();
+
+        Account account = getAccountNumber(accNo);
+
+        if (account != null) {
+            account.displayAccount();
+        }
+        else{
+            System.out.println("Account does not exist");
+        }
+
+
+    }
+    private static Account getAccountNumber(int accountNumber){
+        for (Account account : accounts){
+            if (account.accountNo == accountNumber){
+                return account;
+            }
+        }
+        return null;
+    }
+    private static boolean checkAccountNumber(int accountNumber){
+        for (Account account : accounts){
+            if (account.accountNo == accountNumber) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
